0c833f13f5e07f8a08203d5e5fd1ee70d1a1ead8
maxiums <connectwithadarsha@gmail.com>
2021-06-12 19:09:13 +0545

    chore: configure lint staged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Text, Link, VStack, Code, Grid } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <VStack spacing={8}>
+          <Text>
+            Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+          </Text>
+          <Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
   );
 }
 
