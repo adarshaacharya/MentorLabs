ebe7d18d2064546ca41fec8d7e0c7bb625e19c5a
maxiums <connectwithadarsha@gmail.com>
2021-06-22 01:52:52 +0545

    feat: add hero section

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,3 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import heroImg from './assets/landing.svg';
+
 export const Home = () => {
-  return <h1>Welcome to home page boys</h1>;
+  const navigate = useNavigate();
+  return (
+    <section className="home">
+      <div className="container">
+        <div className="hero">
+          <div className="hero__about">
+            <h1>Mentor Labs.</h1>
+            <p>
+              We are building next generation collaborative learning platform for connecting the mentors and students
+              all around the globe.
+            </p>
+
+            <button onClick={() => navigate('/create-account')} className="hero__signup-btn">
+              Get Started Now
+            </button>
+          </div>
+
+          <div className="hero__img">
+            <img src={heroImg} alt="hero" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
