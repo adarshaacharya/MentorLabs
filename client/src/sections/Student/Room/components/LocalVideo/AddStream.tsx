81024434371318f6fe32b8c8e4d0c4bb7069aab2
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-11 22:52:35 +0545

    Revert "fix: works tags minumum number"

--- client/src/sections/Student/Room/components/LocalVideo/AddStream.tsx
+++
@@ -1,7 +0,0 @@
-export const AddStream: any = ({ stream, connUserSocketId }) => {
-  return (
-    <div className="video_track__container">
-      <video src={stream} autoPlay id={`${connUserSocketId}-video`}></video>
-    </div>
-  );
-};
