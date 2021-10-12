8bd2e47b80681c7afd58b9f1a7ae75d42c2c2ba8
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-12 08:49:05 +0545

    feat: add more tags

--- client/src/data/tags.ts
+++ client/src/data/tags.ts
@@ -1,1 +1,10 @@
-export const tags = ['database', 'networking', 'compiler'];
+export const tags = [
+  'database',
+  'networking',
+  'compiler',
+  'cloud computing',
+  'web  technologies',
+  'data structures and algorithms',
+  'mathematics',
+  'machine learning',
+];
