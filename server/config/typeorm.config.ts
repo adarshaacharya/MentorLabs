2e3e28ad52eff956ae3ed8eba1530aaa34bd40b5
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-23 22:36:52 +0545

    fix: typeorm path for production

--- server/config/typeorm.config.ts
+++ server/config/typeorm.config.ts
@@ -12,6 +12,6 @@ export const ormconfig: ConnectionOptions = {
   database: process.env.DB_NAME,
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test', // in dev : 1 && 1 = 1
-  entities: ['server/modules/**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   namingStrategy: new SnakeNamingStrategy(),
 };
