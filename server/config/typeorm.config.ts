f1e575dd9f9cb4b6ac9642a7b46c60b47cff16e4
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-24 11:36:22 +0545

    refactor: update typeorm config

--- server/config/typeorm.config.ts
+++ server/config/typeorm.config.ts
@@ -5,11 +5,15 @@ const DATABASE_TYPE = 'postgres';
 export const ormconfig: ConnectionOptions = {
   name: 'default',
   type: DATABASE_TYPE,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  ...(process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+      }),
   synchronize: true,
   logging: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test', // in dev : 1 && 1 = 1
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
