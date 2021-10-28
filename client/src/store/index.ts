687978f87814b502b96c82ed09095b0f1a7c3586
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-28 07:57:28 +0545

    refactor: disable non-serializable values

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -4,5 +4,4 @@ import rootReducer from 'store/rootReducer';
 export const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }), // RTK by default doesn't allow to store store and serializable value
 });
